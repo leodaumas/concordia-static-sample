dd2de649ea15ba2e236b9d4caafc4e942634860f
Leo Daumas <leofdaumas@gmail.com>
2020-12-16 15:11:17 +0100

    adding hovers in landing page

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const text = document.querySelectorAll(".text");
 const changeChevron = document.querySelectorAll(".changeChevron");
 const menu = document.querySelector('.menu');
 
+const header = document.querySelector('.stickyMenu');
+const sticky = document.querySelector('.sticky');
+
 for (let i = 0; i < changeChevron.length; i++) {
 	changeChevron[i].addEventListener("click", function() {
 		changeChevron[i].classList.toggle("top");
@@ -21,7 +24,22 @@ if(window.SimpleSlide) {
 if(window.SimpleSlide) {
 	new SimpleAnime();
 }
+// Menu Smartphone
 
 menu.addEventListener('click', function() {
 	this.classList.toggle('open');
 });
+
+
+// Sticky menu
+
+window.onscroll = function() {stickyMenuFunction()};
+
+function stickyMenuFunction() {
+  if (window.pageYOffset > 400) {
+		header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+//
